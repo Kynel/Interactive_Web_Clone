0ee8ae14862fad22dddf16de0669a8e8f99ebe01
kynel <kynel95@gmail.com>
2021-12-01 18:35:27 +0900

    Write JS for scroll event

---
+++ apple-clone-v10/js/blank.js
@@ -0,0 +1,93 @@
+// 전역변수 사용을 피하기 위해, 익명함수 형태로 작성
+(() => {
+  let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
+  const sceneInfo = [
+    {
+      // 0
+      type: 'sticky',
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      // 절댓값으로 미리 지정하지 않는 이유는, 사용자의 디바이스/창 사이즈 변경에 따라 스크롤 높이가 달라질 수 있기 때문.
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-0'),
+      },
+    },
+    {
+      // 1
+      type: 'normal',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-1'),
+      },
+    },
+    {
+      // 2
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-2'),
+      },
+    },
+    {
+      // 3
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-3'),
+      },
+    },
+  ];
+
+  function setLayout() {
+    // 각 스크롤 섹션의 높이 세팅
+    for (let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      sceneInfo[
+        i
+      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+
+    yOffset = window.pageYOffset;
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
+  }
+
+  function scrollLoop() {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return; // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
+      currentScene--;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    }
+  }
+
+  window.addEventListener('scroll', () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  });
+  // window.addEventListener('DOMContentLoaded', setLayout);
+  window.addEventListener('load', setLayout);
+  window.addEventListener('resize', setLayout);
+})();
